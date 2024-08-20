d5dc0a8abaaa65466db719d67f4637905fdaf05d
Fernando <ing.fernandovg@gmail.com>
2024-08-19 20:29:45 -0600

    delete endpoint getOne

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -11,13 +11,6 @@ const create = catchError(async(req, res) => {
     return res.status(201).json(result);
 });
 
-const getOne = catchError(async(req, res) => {
-    const { id } = req.params;
-    const result = await User.findByPk(id);
-    if(!result) return res.sendStatus(404);
-    return res.json(result);
-});
-
 const remove = catchError(async(req, res) => {
     const { id } = req.params;
     const result = await User.destroy({ where: {id} });
@@ -38,7 +31,6 @@ const update = catchError(async(req, res) => {
 module.exports = {
     getAll,
     create,
-    getOne,
     remove,
     update
 }
