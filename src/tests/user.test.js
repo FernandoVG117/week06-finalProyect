d6f6029061868d745280e6d8f4006749e2599c9f
Fernando <ing.fernandovg@gmail.com>
2024-08-20 21:25:29 -0600

    test 02 private endpoint GET user.test.js

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const app = require('../app');
 
 const BASE_URL = '/api/v1/users';
+let userId;
 let TOKEN;
 
 beforeAll(async() => {
@@ -34,6 +35,8 @@ test("POST --> BASE_URL, should return statusCode 201, and res.body.firstName ==
         .post(`${BASE_URL}`)
         .send(user)    
 
+        // console.log(res.body)
+
         expect(res.statusCode).toBe(201);
         expect(res.body).toBeDefined();
 
@@ -45,4 +48,14 @@ test("POST --> BASE_URL, should return statusCode 201, and res.body.firstName ==
             // For the hashed password, onlye verify if been defined.
         expect(res.body.password).toBeDefined()
 
+})
+
+test("GET --> BASE_URL, should return statusCode 200, and res.body.length === 2", async() => {
+    const res = await request(app)
+        .get(BASE_URL)
+        .set('Authorization', `Bearer ${TOKEN}`) 
+
+        expect(res.statusCode).toBe(200)
+        expect(res.body).toBeDefined()
+        expect(res.body).toHaveLength(2)
 })
