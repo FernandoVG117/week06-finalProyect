7623c44d7133a763fa41a1d47aad4089a8a48e0d
Fernando <ing.fernandovg@gmail.com>
2024-08-23 20:37:05 -0600

    model Cart with corrects controllers and routers

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,17 @@
 const User = require('./User');
 const Category = require('./Category');
 const Product = require('./Product');
+const Cart = require('./Cart');
 
     // Product --> categoryId
 Product.belongsTo(Category)
 Category.hasMany(Product)
+
+    // Cart
+        // Cart --> userId
+Cart.belongsTo(User)
+User.hasMany(Cart)
+
+        // Cart --> productId
+Cart.belongsTo(Product)
+Product.hasMany(Cart)
