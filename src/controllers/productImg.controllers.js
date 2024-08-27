4d5f336f2fca75836c4fa8075395333d596413b3
Fernando <ing.fernandovg@gmail.com>
2024-08-26 22:13:25 -0600

    DELETE productImg.controllers.js

--- src/controllers/productImg.controllers.js
+++ src/controllers/productImg.controllers.js
@@ -1,5 +1,7 @@
 const catchError = require('../utils/catchError');
 const ProductImg = require('../models/ProductImg');
+const path = require('path');
+const fs = require('fs')
 
 const getAll = catchError(async(req, res) => {
     const results = await ProductImg.findAll();
@@ -12,21 +14,20 @@ const getAll = catchError(async(req, res) => {
 // });
 
 const create = catchError(async(req, res) => {
-
     const {filename} = req.file;
     // console.log(file)
     const url = `${req.protocol}://${req.headers.host}/uploads/${filename}`;
-
     const result = await ProductImg.create({filename, url});
-
-
     return res.status(201).json(result);
 });
 
 const remove = catchError(async(req, res) => {
     const { id } = req.params;
-    const result = await ProductImg.destroy({ where: {id} });
-    if(!result) return res.sendStatus(404);
+    const image = await ProductImg.findByPk(id);
+    if(!image) return sendStatus(404);
+    const imagePath = path.join(__dirname, '..', 'public', 'uploads', `${image.filename}`)
+    fs.unlinkSync(imagePath) // Stop the ejecution of the instance
+    await image.destroy();
     return res.sendStatus(204);
 });
 
